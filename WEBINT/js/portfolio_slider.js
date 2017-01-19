9cc9d857783cdb0ba7b251f2c0b8a1a3c55b73ac
Thanh An <an.than2015@ict.jvn.edu.vn>
2017-01-20 00:34:38 +0100

    event page

--- WEBINT/js/portfolio_slider.js
+++ WEBINT/js/portfolio_slider.js
@@ -157,3 +157,191 @@ $("#elastic_grid_demo").elastic_grid({
 		]
 	});
 });
+
+$(function(){
+$("#elastic_grid_demo1").elastic_grid({	
+	'hoverDirection': true,
+	'hoverDelay': 0,
+	'hoverInverse': false,
+	'expandingSpeed': 500,
+	'expandingHeight': 500,
+	'items' :
+		[
+			{
+			'title' : 'IRONMAN NICE FRANCE - Nice, France',
+			'description'   : 'A venue with steep triathlon history: swim in the Mediterranean, bike through the alps, run along the Nices historic waterfront',
+			'thumbnail' : ['img/webint-resource/small/nice_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg'],
+			'large' : ['img/webint-resource/large/nice_large.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'View Details', 'url' : 'http://#' },
+			{ 'title':'Download', 'url':'http://#'}
+			],
+			'tags'  : ['All']
+			},
+
+			{
+			'title' : 'IRONMAN WORLD CHAMPIONSHIP- KONA, United State of America',
+			'description'   : 'For over three decades the IRONMAN World Championship has brought the worlds best athletes together in competition.',
+			'thumbnail' : ['img/webint-resource/small/kona_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg'],
+			'large' : ['img/webint-resource/large/kona_large.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://#' },
+			{ 'title':'Download', 'url':'http://#'}
+			],
+			'tags'  : ['All','Joined']
+			},
+
+
+			{
+			'title' : 'IRONMAN Wales- Pembrokeshire, Wales',
+			'description'   : 'A challenging course that takes in views of everything from stunning beaches to Iron Age forts',
+			'thumbnail' : ['img/webint-resource/small/wales_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg'],
+			'large' : ['img/webint-resource/large/wales_large.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://#' },
+			{ 'title':'Download', 'url':'http://#'}
+			],
+			'tags'  : ['All','Past']
+			},
+
+
+			{
+			'title' : 'IRONMAN Barcelona - Calella, Spain',
+			'description'   : 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
+			'thumbnail' : ['img/webint-resource/small/barcelona_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg'],
+			'large' : ['img/webint-resource/large/barcelona_large.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://#' },
+			{ 'title':'Download', 'url':'http://#'}
+			],
+			'tags'  : ['All','Past']
+			},
+			
+		]
+	});
+});
+
+$(function(){
+$("#elastic_grid_demo2").elastic_grid({	
+	'hoverDirection': true,
+	'hoverDelay': 0,
+	'hoverInverse': false,
+	'expandingSpeed': 500,
+	'expandingHeight': 500,
+	'items' :
+		[
+			{
+			'title' : 'IRONMAN NICE FRANCE - Nice, France',
+			'description'   : 'A venue with steep triathlon history: swim in the Mediterranean, bike through the alps, run along the Nices historic waterfront',
+			'thumbnail' : ['img/webint-resource/small/nice_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg'],
+			'large' : ['img/webint-resource/large/nice_large.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'View Details', 'url' : 'http://#' },
+			{ 'title':'Download', 'url':'http://#'}
+			],
+			'tags'  : ['All']
+			},
+
+			{
+			'title' : 'IRONMAN WORLD CHAMPIONSHIP- KONA, United State of America',
+			'description'   : 'For over three decades the IRONMAN World Championship has brought the worlds best athletes together in competition.',
+			'thumbnail' : ['img/webint-resource/small/kona_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg'],
+			'large' : ['img/webint-resource/large/kona_large.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://#' },
+			{ 'title':'Download', 'url':'http://#'}
+			],
+			'tags'  : ['All','Joined']
+			},
+
+
+			{
+			'title' : 'IRONMAN Wales- Pembrokeshire, Wales',
+			'description'   : 'A challenging course that takes in views of everything from stunning beaches to Iron Age forts',
+			'thumbnail' : ['img/webint-resource/small/wales_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg'],
+			'large' : ['img/webint-resource/large/wales_large.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://#' },
+			{ 'title':'Download', 'url':'http://#'}
+			],
+			'tags'  : ['All','Past']
+			},
+
+
+			{
+			'title' : 'IRONMAN Barcelona - Calella, Spain',
+			'description'   : 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.',
+			'thumbnail' : ['img/webint-resource/small/barcelona_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg'],
+			'large' : ['img/webint-resource/large/barcelona_large.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://#' },
+			{ 'title':'Download', 'url':'http://#'}
+			],
+			'tags'  : ['All','Past']
+			},
+
+			{
+			'title' : 'IRONMAN KALMAR - Kalmar, Sweden',
+			'description'   : 'Come, meet the most famous IRONMAN audience in the world! The inaugural IRONMAN event in Scandinavia took place in Kalmar in 2012 and the event has quickly become an athlete favorite. The swim course is in the Baltic Sea, the bike course is over the 6 km long bridge to the island Öland, a popular spot for tourists from around the world, and the run course goes through central parts of Kalmar with the finish line located on the main square.',
+			'thumbnail' : ['img/webint-resource/small/kalmar_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg'],
+			'large' : ['img/webint-resource/large/kalmar_large.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://#' },
+			{ 'title':'Download', 'url':'http://#'}
+			],
+			'tags'  : ['All','Past']
+			},
+
+			{
+			'title' : 'IRONMAN Austria-Kärnten - Klagenfurt, Austria',
+			'description'   : 'Swim in crystal clear waters, then bike and run through the heart of Carinthia and Klagenfurt.',
+			'thumbnail' : ['img/webint-resource/small/austria_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg'],
+			'large' : ['img/webint-resource/large/austria_large.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://#' },
+			{ 'title':'Download', 'url':'http://#'}
+			],
+			'tags'  : ['All', 'Past']
+			},
+
+
+			{
+			'title' : 'IRONMAN 70.3 Vichy - Auvergne, France',
+			'description'   : 'Race in one of France most beautiful cities. Vichy, known as the "Queen of Spa Towns" offers a trip filled with relaxation, fun, art and great racing.',
+			'thumbnail' : ['img/webint-resource/small/vichy_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg'],
+			'large' : ['img/webint-resource/large/vichy_large.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://#' },
+			{ 'title':'Download', 'url':'http://#'}
+			],
+			'tags'  : ['All' , 'Joined']
+			},
+
+			{
+			'title' : 'IRONMAN Copenhagen - Copenhagen, Denmark',
+			'description'   : 'One of Europes most beautiful and energetic cities plays host to a day of swimming, cycling, and running.',
+			'thumbnail' : ['img/webint-resource/small/copenhagen_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg', 'img/webint-resource/small/vietnam_small.jpg'],
+			'large' : ['img/webint-resource/large/copenhagen_large.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg', 'img/webint-resource/large/vietnam_small.jpg'],
+			'button_list'   :
+			[
+			{ 'title':'Demo', 'url' : 'http://#' },
+			{ 'title':'Download', 'url':'http://#'}
+			],
+			'tags'  : ['All']
+			},
+
+		
+		]
+	});
+});
